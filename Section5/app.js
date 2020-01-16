f165b5c260ab9e26600854ebabaaad918892b30e
Venugopal Reddy Devarapally <venu.d@westagilelabs.com>
2020-01-16 19:34:13 +0530

    Awesome blossom

---
+++ Section5/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const path = require('path');
+const rootDir = require('./util/path');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, '..', 'views', 'page-not-found.html'));
+});
+
+app.listen(3000);
