eec5308fb0d7bb4579284eef9515b7330d61103e
Venugopal Reddy Devarapally <venu.d@westagilelabs.com>
2020-01-20 17:00:45 +0530

    Pug and templating

--- Section5/app.js
+++ Section5/app.js
@@ -4,17 +4,20 @@ const path = require('path');
 const rootDir = require('./util/path');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, '..', 'views', 'page-not-found.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
